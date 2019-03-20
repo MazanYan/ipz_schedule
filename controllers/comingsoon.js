d4e06e49c939947e773b402efeb0a4d9fbcab91a
Gemofort <vanya6677@gmail.com>
2019-03-20 20:17:16 +0200

    Small changes

--- controllers/comingsoon.js
+++ controllers/comingsoon.js
@@ -4,9 +4,6 @@ const absPath = path.dirname(__dirname);
 
 const loadComingSoon = (req, res) => {
   res.sendFile(path.join(absPath, '/views/comingsoon.html'));
-  setTimeout(() => {
-    res.sendFile(path.join(absPath, '/views/index.html'))
-  }, 1000);
 };
 
 module.exports = { loadComingSoon };
